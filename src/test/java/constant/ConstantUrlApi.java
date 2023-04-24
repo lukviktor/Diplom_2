dac409c5fb7ec5dc0adcfa69d5e4f26e16b8896d
Viktor Lukashev <lukashevviktor@gmail.com>
2023-04-24 10:56:51 +0300

    Добавили константы API

---
+++ src/test/java/constant/ConstantUrlApi.java
@@ -0,0 +1,8 @@
+package constant;
+
+public class ConstantUrlApi {
+    public final static String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+    public final static String ENDPOINT_REGISTER_USER = "api/auth/register";
+    public final static String ENDPOINT_LOGIN_USER = "api/auth/login";
+    public final static String ENDPOINT_DELETE_USER = "api/auth/user";
+}
